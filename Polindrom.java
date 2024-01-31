970c6540826c57905f4f51bf3690713a23bb4e9b
Hamza Eren <130919122+Hamza-Eren@users.noreply.github.com>
2024-01-31 22:59:33 +0300

    Add files via upload

---
+++ Polindrom.java
@@ -0,0 +1,27 @@
+// @author HamzaEren
+package github;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Polindrom {
+    static boolean isPol(String word){
+        String str= word;
+        String nstr="";
+        char ch;
+        
+        for (int i=0; i<str.length(); i++){
+            ch = str.charAt(i);
+            nstr = ch+nstr;
+        }
+        return nstr.toLowerCase().equals(word.toLowerCase());
+    }
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Kelime: ");
+        String word = scan.nextLine();
+        System.out.println(isPol(word));
+    }
+}
