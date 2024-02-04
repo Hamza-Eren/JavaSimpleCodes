961e048a65e513ea4e84193d0ada018dd1b76e53
Hamza Eren <130919122+Hamza-Eren@users.noreply.github.com>
2024-02-04 16:32:47 +0300

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -4,7 +4,7 @@ package github;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Polindrom {
+public class Palindrome {
     static boolean isPol(String word){
         String str= word;
         String nstr="";
